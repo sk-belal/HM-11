d58ae6e2b20bcd776c1de9321b48efe2c2d6f03e
Sheikh Ahmed Belal <134007028+sk-belal@users.noreply.github.com>
2023-08-13 14:49:56 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+   function(){
+    fanPower = 10;   // watts
+ lightPower = 5;  // watts
+ fridgePower = 50; // watts
+
+// Cost per watt in taka
+ costPerWatt = 500; // taka
+
+// Calculate the total power consumption
+ totalPower = fanPower*2 + lightPower + fridgePower;
+
+// Calculate the total cost
+ totalCost = totalPower * costPerWatt;
+
+// Display the result
+console.log(`Total power  ${totalPower} watts`);
+console.log(`Total cost: ${totalCost} taka`);
+
+   } // Given power consumption in watts
+ 
+ 
+
+
+
+
